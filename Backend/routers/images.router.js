68f557b1a85c28c57b676a22f591150e170445c1
Patrik Bajkusa <patrikb99@hotmail.com>
2024-03-10 21:20:05 +0100

    added the saving to unique id

--- Backend/routers/images.router.js
+++ Backend/routers/images.router.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
-const {
-  getAllpictures,
-  sendInPictures,
-} = require("../controller/images.controller");
-
 const router = express.Router();
+const users = [];
 
-router.post("/:id", sendInPictures);
-router.get("/:id", getAllpictures);
+router.get("/:id", (req, res) => {
+  const userId = req.params.id;
+  res.send(users.filter((user) => user.userId === userId));
+});
+router.post("/:id", (req, res) => {
+  users.push(req.body);
+  res.status(201).json(users);
+});
 
 module.exports = router;
